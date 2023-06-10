a9b9b797753c31ce2819f064ba6a734d8c73b1db
Saim Ashfaq <saim.ashfaq@codedistrict.com>
2023-06-10 19:16:08 +0500

    Removed unused imports from user dto

--- src/modules/users/dto/getUsers.dto.ts
+++ src/modules/users/dto/getUsers.dto.ts
@@ -1,17 +1,4 @@
-import {
-  IsArray,
-  IsEmail,
-  IsEnum,
-  IsNotEmpty,
-  IsNumber,
-  IsString,
-  IsUUID,
-  Matches,
-  MaxLength,
-  MinLength,
-  ValidateIf,
-  isNotEmpty,
-} from 'class-validator';
+import { IsEmail, IsNumber, IsString } from 'class-validator';
 
 export class GetUserRequestDto {
   @IsString()
