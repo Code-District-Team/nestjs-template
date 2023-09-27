2ddef391b33b42883d28e507e4f02f301a50f3ce
Bilal Ahmad <bilal.ahmad@codedistrict.com>
2023-09-27 15:57:15 +0500

    parse float price column

--- src/modules/product/entities/product.entity.ts
+++ src/modules/product/entities/product.entity.ts
@@ -1,5 +1,16 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, } from 'typeorm';
 
+export class ColumnNumericTransformer {
+  to(data: number): number {
+    return data;
+  }
+
+  from(data: string): number {
+    return parseFloat(data);
+  }
+}
+
+
 @Entity({ name: 'products' })
 export class Product {
   @PrimaryGeneratedColumn('uuid')
@@ -8,7 +19,7 @@ export class Product {
   @Column({ length: 250 })
   name: string;
 
-  @Column({ precision: 10, scale: 2, type: "decimal" })
+  @Column({ precision: 10, scale: 2, type: "decimal", transformer: new ColumnNumericTransformer() })
   price: number;
 
   @Column({ type: "int" })
