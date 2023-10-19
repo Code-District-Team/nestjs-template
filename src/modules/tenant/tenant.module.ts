1590a98cafba218c25232cce8623cb06132ffed7
Bilal Ahmad <bilal.ahmad@codedistrict.com>
2023-10-19 16:43:28 +0500

    tenant users listing

--- src/modules/tenant/tenant.module.ts
+++ src/modules/tenant/tenant.module.ts
@@ -3,9 +3,10 @@ import { TenantService } from './tenant.service';
 import { TenantController } from './tenant.controller';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Tenant } from "./entities/tenant.entity";
+import { User } from "../users/entities/user.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tenant])],
+  imports: [TypeOrmModule.forFeature([Tenant, User])],
   providers: [TenantService],
   controllers: [TenantController],
   exports: [TenantService]
