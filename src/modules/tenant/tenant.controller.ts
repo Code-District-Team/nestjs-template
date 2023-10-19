487b60d6ac1af4354dd048dbf3ec609dcf367909
Bilal Ahmad <bilal.ahmad@codedistrict.com>
2023-10-19 16:31:38 +0500

    tenant user listing

--- src/modules/tenant/tenant.controller.ts
+++ src/modules/tenant/tenant.controller.ts
@@ -1,18 +1,19 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
 import { TenantService } from "./tenant.service";
 
 @Controller('tenant')
 export class TenantController {
-  constructor(private readonly tenantService: TenantService) {}
-
-
-  @Post()
-  create() {
-
+  constructor(private readonly tenantService: TenantService) {
   }
+
 
   @Get()
   findAll() {
     return this.tenantService.findAll();
+  }
+
+  @Get(":id")
+  findOne(@Param("id", ParseUUIDPipe) id: string) {
+    return this.tenantService.findOne(id);
   }
 }
